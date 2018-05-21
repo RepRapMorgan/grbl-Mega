0af53a1263df883759b0df417f27118b59d43bae
Felix Schill <felix.schill@hydromea.com>
2018-05-21 18:14:16 +0200

    changed settings back to 5-axis

--- upload.sh
+++ upload.sh
@@ -1,1 +1,3 @@
-~/programs/arduino-1.6.13/hardware/tools/avr/bin/avrdude -C ~/programs/arduino-1.6.13/hardware/tools/avr/etc/avrdude.conf -v -patmega2560 -Uflash:w:grbl.hex:i -cwiring  -P /dev/ttyUSB0 -b115200
+~/programs/arduino-1.8.5/hardware/tools/avr/bin/avrdude -C ~/programs/arduino-1.8.5/hardware/tools/avr/etc/avrdude.conf -v -patmega2560 -D -Uflash:w:grbl.hex:i -cwiring  -P $1 -b115200
+
+
