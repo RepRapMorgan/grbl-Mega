5548bb5db2a1333eba4c1b17e2385ae8fdd7cd82
Felix Schill <felix.schill@hydromea.com>
2018-07-30 21:18:29 +0200

    added angle parameter to dwell command to wait for a specific spindle rotation (fixed bug)

--- grbl/spindle_encoder.c
+++ grbl/spindle_encoder.c
@@ -26,7 +26,7 @@ void spindle_encoder_init() {
 float spindle_get_relative_angle() {
 	uint32_t now = get_timer_ticks();
     // get rough angle based on encoder ticks
-    float angle = 360.0 * spindle_encoder.current_encoder_count/(float)spindle_encoder.ticks_per_rev;
+    float angle = 360.0 * (spindle_encoder.current_encoder_count%spindle_encoder.ticks_per_rev);
     angle += ((uint32_t)spindle_encoder.speed * (now - spindle_encoder.last_tick_time)) * 60.0 / spindle_encoder.ticks_per_rev / 1000000;
     return angle;
 }
